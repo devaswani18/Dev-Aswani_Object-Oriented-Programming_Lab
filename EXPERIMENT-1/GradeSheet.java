425e6ccc61e4a2bc6930619271d7345c64e19a1b
devaswani18 <aswanidev997@gmail.com>
2025-05-02 18:18:46 +0530

    Add files via upload

---
+++ EXPERIMENT-1/GradeSheet.java
@@ -0,0 +1,15 @@
+public class GradeSheet {
+    public static void main(String[] args) {
+        System.out.println("*************************");
+        System.out.println("*      GRADE SHEET      *");
+        System.out.println("*************************");
+        System.out.println("Name: John Doe");
+        System.out.println("Course: Computer Science");
+        System.out.println("\nSubjects\tMarks");
+        System.out.println("------------------------");
+        System.out.println("Math\t\t90");
+        System.out.println("Science\t\t85");
+        System.out.println("English\t\t88");
+        System.out.println("*************************");
+    }
+}
---
+++ EXPERIMENT-1/MyDetails.java
@@ -0,0 +1,6 @@
+public class MyDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: John Doe");
+        System.out.println("Address: 123 Main Street, City, Country");
+    }
+}
